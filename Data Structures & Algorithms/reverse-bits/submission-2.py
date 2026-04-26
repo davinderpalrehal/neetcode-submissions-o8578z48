a54a377f48e4980290acf92cde4b7eace0c6da21
Davinderpal Singh Rehal <davinderpalrehal@gmail.com>
2026-04-26 20:59:08 +0200

    Add: reverse-bits - submission-2

---
+++ Data Structures & Algorithms/reverse-bits/submission-2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        n_str = bin(n)[2:]
+        res = ''
+
+        for c in n_str:
+            res = c + res
+        
+        while len(res) < 32:
+            res += '0'
+        
+        return int(res, 2)
